fd00d0b3efc8e80446bad1266ca89b9170a63061
anish <shobhashama9@gmail.com>
2022-10-22 00:14:08 +0530

    Update bot.mjs

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,9 +1,10 @@
 import { BotClient } from "./structures/BotClient.mjs";
+//<-----------------Imported Modules----------------->
 // https://discord.com/api/oauth2/authorize?client_id=1032998523123290182&permissions=2192919040&scope=bot
 const client = new BotClient();
 
 client.on("TomatoBot6966Loaded", () => {
     client.logger.info("Now starting the bot");
-    
+    //<-----------------Login in bot----------------->
     client.login(process.env.DISCORD_TOKEN)
 })
